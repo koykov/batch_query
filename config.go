b16d857288a44d4afb054cc1d9b7de6eb5c717d7
Konstantin Voykov <koykov0@gmail.com>
2023-09-02 22:24:06 +0300

    Comment issues: config.

--- config.go
+++ config.go
@@ -12,16 +12,33 @@ const (
 	defaultBuffer          = 16
 )
 
+// Config describes query properties and behavior.
 type Config struct {
-	BatchSize       uint64
+	// Max capacity of one batch.
+	// If query collects that amount of requests before reach CollectInterval then batch will process with reason
+	// 'size reach'.
+	// If this param omit defaultBatchSize (64) will use instead.
+	BatchSize uint64
+	// How long collect requests before process the batch.
+	// Timer starts by first request incoming and stops after process the batch. After reach that interval betch will
+	// process even contains one request.
+	// If this param omit defaultCollectInterval (1 second) will use instead.
 	CollectInterval time.Duration
+	// How long request may wait collecting and processing. Must be greater that CollectInterval.
 	TimeoutInterval time.Duration
-	Workers         uint
-	Buffer          uint64
-	Batcher         Batcher
+	// Internal workers count to process batches.
+	Workers uint
+	// Internal buffer size to collect batches.
+	// If this param omit defaultBuffer (16) will use instead.
+	Buffer uint64
+	// Batch processor.
+	// Mandatory param.
+	Batcher Batcher
 
+	// Metrics writer handler.
 	MetricsWriter MetricsWriter
 
+	// Logger handler.
 	Logger Logger
 }
 
