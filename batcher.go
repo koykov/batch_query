edc2f8aa26676335c75c3a604ce329adb4b3e1c6
Konstantin Voykov <koykov0@gmail.com>
2023-09-03 22:45:47 +0300

    Comment issues: batcher.

--- batcher.go
+++ batcher.go
@@ -2,7 +2,10 @@ package batch_query
 
 import "context"
 
+// Batcher describes object to process batches.
 type Batcher interface {
+	// Batch processes collected batch.
 	Batch(dst []any, keys []any, ctx context.Context) ([]any, error)
+	// MatchKey checks if key corresponds to val.
 	MatchKey(key, val any) bool
 }
