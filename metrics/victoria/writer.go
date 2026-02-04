ea27788a6bfe21bd2a8e8bc8c39981ccaa5846db
Konstantin Voykov <koykov0@gmail.com>
2026-02-04 22:48:16 +0300

    metrics: victoria: make implementation private and declare interface

--- metrics/victoria/writer.go
+++ metrics/victoria/writer.go
@@ -19,14 +19,28 @@ const (
 	ioFail = "fail"
 )
 
-// Writer is a VictoriaMetrics implementation of batch_query.MetricsWriter.
-type Writer struct {
+type Writer interface {
+	Fetch()
+	OK(duration time.Duration)
+	NotFound()
+	Timeout()
+	Interrupt()
+	Fail()
+	Batch()
+	BatchOK(duration time.Duration)
+	BatchFail()
+	BufferIn(reason string)
+	BufferOut()
+}
+
+// writer is a VictoriaMetrics implementation of batch_query.MetricsWriter.
+type writer struct {
 	name string
 	prec time.Duration
 }
 
-func NewWriter(name string, options ...Option) *Writer {
-	w := &Writer{
+func NewWriter(name string, options ...Option) Writer {
+	w := &writer{
 		name: name,
 		prec: time.Nanosecond,
 	}
@@ -39,59 +53,59 @@ func NewWriter(name string, options ...Option) *Writer {
 	return w
 }
 
-func (m Writer) Fetch() {
+func (m writer) Fetch() {
 	vmchain.Gauge("batch_query_size", nil).WithLabel("query", m.name).WithLabel("entity", single).Inc()
 	vmchain.Counter("batch_query_io").WithLabel("query", m.name).WithLabel("entity", single).WithLabel("type", ioIn).Inc()
 }
 
-func (m Writer) OK(dur time.Duration) {
+func (m writer) OK(dur time.Duration) {
 	vmchain.Gauge("batch_query_size", nil).WithLabel("query", m.name).WithLabel("entity", single).Dec()
 	vmchain.Counter("batch_query_io").WithLabel("query", m.name).WithLabel("entity", single).WithLabel("type", ioOK).Inc()
 	vmchain.Histogram("batch_query_timing").WithLabel("query", m.name).WithLabel("entity", single).Update(float64(dur / m.prec))
 }
 
-func (m Writer) NotFound() {
+func (m writer) NotFound() {
 	vmchain.Gauge("batch_query_size", nil).WithLabel("query", m.name).WithLabel("entity", single).Dec()
 	vmchain.Counter("batch_query_io").WithLabel("query", m.name).WithLabel("entity", single).WithLabel("type", io404).Inc()
 }
 
-func (m Writer) Timeout() {
+func (m writer) Timeout() {
 	vmchain.Gauge("batch_query_size", nil).WithLabel("query", m.name).WithLabel("entity", single).Dec()
 	vmchain.Counter("batch_query_io").WithLabel("query", m.name).WithLabel("entity", single).WithLabel("type", ioTO).Inc()
 }
 
-func (m Writer) Interrupt() {
+func (m writer) Interrupt() {
 	vmchain.Gauge("batch_query_size", nil).WithLabel("query", m.name).WithLabel("entity", single).Dec()
 	vmchain.Counter("batch_query_io").WithLabel("query", m.name).WithLabel("entity", single).WithLabel("type", ioInt).Inc()
 }
 
-func (m Writer) Fail() {
+func (m writer) Fail() {
 	vmchain.Gauge("batch_query_size", nil).WithLabel("query", m.name).WithLabel("entity", single).Dec()
 	vmchain.Counter("batch_query_io").WithLabel("query", m.name).WithLabel("entity", single).WithLabel("type", ioFail).Inc()
 }
 
-func (m Writer) Batch() {
+func (m writer) Batch() {
 	vmchain.Gauge("batch_query_size", nil).WithLabel("query", m.name).WithLabel("entity", batch).Inc()
 	vmchain.Counter("batch_query_io").WithLabel("query", m.name).WithLabel("entity", batch).WithLabel("type", ioIn).Inc()
 }
 
-func (m Writer) BatchOK(dur time.Duration) {
+func (m writer) BatchOK(dur time.Duration) {
 	vmchain.Gauge("batch_query_size", nil).WithLabel("query", m.name).WithLabel("entity", batch).Dec()
 	vmchain.Counter("batch_query_io").WithLabel("query", m.name).WithLabel("entity", batch).WithLabel("type", ioOK).Inc()
 	vmchain.Histogram("batch_query_timing").WithLabel("query", m.name).WithLabel("entity", batch).Update(float64(dur / m.prec))
 }
 
-func (m Writer) BatchFail() {
+func (m writer) BatchFail() {
 	vmchain.Gauge("batch_query_size", nil).WithLabel("query", m.name).WithLabel("entity", batch).Dec()
 	vmchain.Counter("batch_query_io").WithLabel("query", m.name).WithLabel("entity", batch).WithLabel("type", ioFail).Inc()
 }
 
-func (m Writer) BufferIn(reason string) {
+func (m writer) BufferIn(reason string) {
 	vmchain.Gauge("batch_query_size", nil).WithLabel("query", m.name).WithLabel("entity", buffer).Inc()
 	vmchain.Counter("batch_query_bufio").WithLabel("query", m.name).WithLabel("reason", reason).Inc()
 }
 
-func (m Writer) BufferOut() {
+func (m writer) BufferOut() {
 	vmchain.Gauge("batch_query_size", nil).WithLabel("query", m.name).WithLabel("entity", buffer).Dec()
 }
 
